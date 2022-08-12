e0a753bffb0482d2f0c4f33f802ab22474f22762
soychun <110978406+soychun@users.noreply.github.com>
2022-08-12 15:13:09 +0900

    Create S_self_2447_별찍기.py

---
+++ 재귀/S_self_2447_별찍기.py
@@ -0,0 +1,42 @@
+import sys
+n = int(sys.stdin.readline())
+first = ['***','* *','***']
+
+def star(n,x):
+    out = []
+    if n==3:
+        return x
+    else:
+        for i in x:
+            out.append(i*3)
+        for i in x:
+            out.append(i+' '*len(x)+i)
+        for i in x:
+            out.append(i*3)
+        return star(n//3,out)
+
+final = star(n,first)
+for i in final:
+    print(i)
+
+
+'''a = []
+for i in first:
+    print(i*3)
+    a.append(i*3)
+for i in first:
+    print(i+'   '+i)
+    a.append(i+'   '+i)
+for i in first:
+    print(i*3)
+    a.append(i * 3)
+for i in a:
+    print(i)
+second =a
+
+for i in second:
+    print(i*3)
+for i in second:
+    print(i+'   '*3+i)
+for i in second:
+    print(i*3)'''
